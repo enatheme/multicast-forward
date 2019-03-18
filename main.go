23331522c13c0a7dafce25e2f6b94e250fbd77e4
enatheme <enatheme@enatheme.org>
2019-03-18 21:48:26 +0800

    add regex

--- main.go
+++ main.go
@@ -6,9 +6,35 @@ import (
     "log"
     "net"
     "os"
+    "regexp"
     "strings"
     "strconv"
 )
+
+func validate_ip(ip string) (found bool) {
+    // regex from https://stackoverflow.com/questions/13145397/regex-for-multicast-ip-address
+    ip_regex := regexp.MustCompile(`^2(?:2[4-9]|3\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d?|0)){3}:[0-9]+$`)
+    return ip_regex.MatchString(ip)
+}
+
+func validate_input(input string) {
+    input = strings.TrimSuffix(input, "\n")
+    input_split := strings.Split(input, " ")
+    if (!validate_ip(input_split[0])) {
+        log.Fatal("Ip %s didn't pass regex", input_split[0])
+        return
+    }
+    if (!validate_ip(input_split[1])) {
+        log.Fatal("Ip %s didn't pass regex", input_split[1])
+        return
+    }
+    port, err := strconv.Atoi(input_split[2])
+    if err != nil {
+        log.Fatal(err)
+        return
+    }
+    go forward(input_split[0], input_split[1], port)
+}
 
 func forward(addr_to_listen string, addr_to_send string, datagram_size int) {
     addr_listen, err := net.ResolveUDPAddr("udp", addr_to_listen)
@@ -45,8 +71,6 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
     for {
         input, _ := reader.ReadString('\n')
-        input_split := strings.Split(input, " ")
-        port, _ := strconv.Atoi(input_split[2])
-        go forward(input_split[0], input_split[1], port)
+        validate_input(input)
     }
 }
